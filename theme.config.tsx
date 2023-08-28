7e5e51bb2e52de2ba7a644e486316ddfeb6e45fd
Ziv <ziv@Zivs-MacBook-Pro.local>
2023-08-28 22:05:28 +0800

    add logo

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+import Logo from './logo.png'
 
 const config: DocsThemeConfig = {
-  logo: <span>TextOps</span>,
+  logo: <span><Image alt="logo" src={Logo} height={42} style={{borderRadius: '3px'}}/></span>,
   project: {
     link: 'https://github.com/wewoor/textops-web',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  // docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
   footer: {
     text: 'TextOps © 2023',
   },
