bb69d3198d297fb06aa2a07b09d19131afadf28b
wewoor <wewoor@gmail.com>
2023-08-31 18:09:11 +0800

    integrate the price

--- components/price/index.tsx
+++ components/price/index.tsx
@@ -17,15 +17,21 @@ export const Price: React.FC<{}> = () => {
         <div className="h-[500px] mt-6 mb-10">
             <h2 className="text-center text-3xl pt-3 pb-8 font-bold">产品价格</h2>
             <div>
-                <script
+                {/* <script
                     async
                     src="https://js.stripe.com/v3/pricing-table.js"
                 ></script>
                 <stripe-pricing-table
                     pricing-table-id="prctbl_1NiBTNIUTzTMDrssOPeZN06m"
                     publishable-key="pk_live_51NXJ2pIUTzTMDrsskPwM3PXzTHP7FsK2q8qy4SJoZ03AwKMBhKePdAvtDHAEi1sKCzy6YHrsDn3NKEjd30QsRG6200qEYYAwaI"
-                ></stripe-pricing-table>
+                ></stripe-pricing-table> */}
+<script async src="https://js.stripe.com/v3/pricing-table.js"></script>
+<stripe-pricing-table pricing-table-id="prctbl_1NkIJ1IUTzTMDrsssQVBqhfp"
+publishable-key="pk_test_51NXJ2pIUTzTMDrssCWNtZebiCEQjrGc9zqTzPuHIdeYtSDog88Jiu4Dm7wGDdOEbF5IGvpijyQGDHV9blH6V5I8Z000T69LMdm">
+</stripe-pricing-table>
             </div>
         </div>
     );
+
+    
 };
