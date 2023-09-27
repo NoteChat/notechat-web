df243cdce92035142d1d3f6e80387909d7fae2b6
Ziv <wewoor@gmail.com>
2023-09-27 20:11:02 +0800

    Update links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,35 +1,46 @@
-import React from 'react'
-import { useTranslation } from 'react-i18next'
-import style from './style.module.css'
-import Link from 'next/link';
+import React from "react";
+import { useTranslation } from "react-i18next";
+import style from "./style.module.css";
+import Link from "next/link";
 
 export const Footer: React.FC<{}> = () => {
     const { t } = useTranslation();
-    const footerLinks = [{
-        title: t('Tutorials'),
-        href: '/document/tutorials',
-    },
-    {
-        title: 'Github',
-        href: 'https://github.com/notechat/notechat-web',
-    }, {
-        title: t('Prompt Templates'),
-        href: '/document/tutorials',
-    },
-    {
-        title: t('Contact'),
-        href: 'mailto:support@notechat.xyz'
-    }
-    ]
+    const footerLinks = [
+        {
+            title: t("Tutorials"),
+            href: "/document/tutorials",
+        },
+        {
+            title: "Github",
+            href: "https://github.com/notechat/notechat-web",
+        },
+        {
+            title: t("Blog"),
+            href: "/blog",
+        },
+        {
+            title: t("Prompt Templates"),
+            href: "/document/tutorials",
+        },
+        {
+            title: t("Contact"),
+            href: "mailto:support@notechat.xyz",
+        },
+    ];
 
     return (
         <div className={style.footer}>
-            {
-                footerLinks.map((link, index) => (
-                    <Link key={index} href={link.href} target="_blank" rel="noopener noreferrer" className='hover:underline'>
-                        {link.title}
-                    </Link>
-                ))}
+            {footerLinks.map((link, index) => (
+                <Link
+                    key={index}
+                    href={link.href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="hover:underline"
+                >
+                    {link.title}
+                </Link>
+            ))}
         </div>
-    )
-}
+    );
+};
