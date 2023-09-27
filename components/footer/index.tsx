99c9a536a4e982eca37cc53044f223e434215dec
Ziv <wewoor@gmail.com>
2023-09-27 19:25:19 +0800

    fix: links

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,26 +1,34 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
+import style from './style.module.css'
+import Link from 'next/link';
 
 export const Footer: React.FC<{}> = () => {
     const { t } = useTranslation();
-    const footerLinks = [
-        {
-            title: 'Github',
-            href: 'https://github.com/notechat/notechat-web',
-        },
-        {
-            title: t('Contact'),
-            href: 'mailto:support@notechat.xyz'
-        }
+    const footerLinks = [{
+        title: t('Tutorials'),
+        href: '/document/tutorials',
+    },
+    {
+        title: 'Github',
+        href: 'https://github.com/notechat/notechat-web',
+    }, {
+        title: t('Prompt Templates'),
+        href: '/document/tutorials',
+    },
+    {
+        title: t('Contact'),
+        href: 'mailto:support@notechat.xyz'
+    }
     ]
 
     return (
-        <div className="w-full flex gap-2 justify-end">
+        <div className={style.footer}>
             {
                 footerLinks.map((link, index) => (
-                    <a key={index} href={link.href} target="_blank" rel="noopener noreferrer" className='hover:underline'>
+                    <Link key={index} href={link.href} target="_blank" rel="noopener noreferrer" className='hover:underline'>
                         {link.title}
-                    </a>
+                    </Link>
                 ))}
         </div>
     )
